32e5cf70a5c166a877fdd21fc55bce57642201cc
tanishqravula <121391761+tanishqravula@users.noreply.github.com>
2023-07-18 09:18:59 +0530

    Add files via upload

---
+++ Diabetes-Prediction-System/Tabs/about.py
@@ -0,0 +1,11 @@
+"""This modules contains data about about page"""
+
+# Import necessary modules
+import streamlit as st
+from PIL import Image
+
+
+def app():
+    """This function creates the about page"""
+    st.balloons()
+    st.title('Contact Us')
